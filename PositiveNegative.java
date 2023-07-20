170015fefd52d34693ece38f660942ae9597a1a5
Sheelanaren <139891973+Sheelanaren@users.noreply.github.com>
2023-07-20 19:57:47 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class PositiveNegative {
+
+	public static void main(String[] args) {
+		int num=35;
+		if (num>0)
+		{
+			System.out.println("Positive Number");
+		}
+		else if (num<0)
+		{
+			System.out.println("Negative Number");
+		}
+		else 
+		{
+			System.out.println("The number is neither positive nor negative");
+		}
+	}
+
+}
