8557212d72cbac7ac72080b2929ac16fb415a264
Sheelanaren <139891973+Sheelanaren@users.noreply.github.com>
2023-07-21 19:59:23 +0530

    Add files via upload

---
+++ Arithmeticoperator.java
@@ -0,0 +1,33 @@
+package week1.day2;
+
+public class Arithmeticoperator {
+
+	public static void main(String[] args) {
+		System.out.println(10+10);
+		System.out.println(5*8);
+		System.out.println(10/5);
+		System.out.println(10-8);
+		System.out.println(10%2);
+		
+		
+	System.out.println("*****************");
+	
+	System.out.println(5<6);
+	System.out.println(5>7);
+	System.out.println(6>=4);
+	System.out.println(8!=7);
+	
+	System.out.println((3<=3)&&(4>6));
+	System.out.println((4>6)||(6>9));
+	
+	int i=1;
+	System.out.println(i++);
+	System.out.println(i);
+	System.out.println(i--);
+	System.out.println(i);
+	
+	
+
+	}
+
+}
